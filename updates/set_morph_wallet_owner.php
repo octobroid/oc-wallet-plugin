6e3240584a8d7296aee54ce1b2898fe98bb55118
Prasetyo Iman Nugroho <pras_iman@hotmail.com>
2019-09-17 15:00:39 +0700

    Fix migration class name

--- updates/set_morph_wallet_owner.php
+++ updates/set_morph_wallet_owner.php
@@ -4,7 +4,7 @@ use Schema;
 use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 
-class ModifyRelatedColumnsOnLogsTable extends Migration
+class SetMorphWalletOwner extends Migration
 {
     public function up()
     {
